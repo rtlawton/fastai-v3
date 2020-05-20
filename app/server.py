2a6a59549418aff444d8ad0d391cd1153df55d63
rtlawton <57228974+rtlawton@users.noreply.github.com>
2020-05-20 20:22:54 +0200

    update server.py
    Customised for blood cells

--- app/server.py
+++ app/server.py
@@ -9,10 +9,12 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/open?id=1RFRWvrLfIQ9KkFOrY_BXWyE_iXYx2Of_'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
+classes = ['EOSINOPHIL', 'LYMPHOCYTE', 'MONOCYTE', 'NEUTROPHIL']
 path = Path(__file__).parent
 
 app = Starlette()
